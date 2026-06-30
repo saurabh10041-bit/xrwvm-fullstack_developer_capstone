dd49c633d9f72f827cffff4d0e07d2a665ed0249
saurabh10041-bit <saurabh10041@gmail.com>
2026-06-30 13:12:14 -0400

    Completed About and Contact pages

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,8 +19,18 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
+
+    path('', TemplateView.as_view(template_name='Home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='About.html'), name='about'),
+    path('contact/', TemplateView.as_view(template_name='Contact.html'), name='contact'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
